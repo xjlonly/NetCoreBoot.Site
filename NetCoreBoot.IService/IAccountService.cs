179998cf13d5114d867f5f00f7ad4bf68fc46227
xjlonly <xjlonly@foxmail.com>
2016-12-22 14:40:03 +0800

    构建服务依赖注入框架

---
+++ NetCoreBoot.IService/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCoreBoot.IServiceFactory;
+
+namespace NetCoreBoot.IService
+{
+    public interface IAccountService : IAppService
+    {
+        bool CheckLogin(string userName, string password);
+    }
+}
---
+++ NetCoreBoot.IServiceFactory/IAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBoot.IServiceFactory
+{
+    public interface IAppService : IDisposable
+    {
+    }
+}
