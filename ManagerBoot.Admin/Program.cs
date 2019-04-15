189292521d0bace9b6d4c4f347e9ed56d278ceae
xjlonly <xjlonly@foxmail.com>
2019-04-15 16:46:08 +0800

    优化结构

--- ManagerBoot.Admin/Program.cs
+++ ManagerBoot.Admin/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace ManagerBoot.Admin
 {
@@ -11,16 +14,27 @@ namespace ManagerBoot.Admin
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseUrls("http://*:5000")
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseContentRoot(Directory.GetCurrentDirectory())
+            //    .UseIISIntegration()
+            //    .UseUrls("http://*:5000")
+            //    .UseStartup<Startup>()
+            //    .UseApplicationInsights()
+            //    .Build();
 
-            host.Run();
+            //host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); //清除日志提供程序 引入第三方日志组件时使用
+                    logging.SetMinimumLevel(LogLevel.Information); //设置日志级别为info
+                    logging.AddConsole(); //添加日志控制程序——控制台上
+                }).UseNLog();
     }
 }
--- ManagerBoot.Admin/Startup.cs
+++ ManagerBoot.Admin/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using System.Reflection;
 using NetCoreBoot.Common;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace ManagerBoot.Admin
 {
@@ -15,13 +16,20 @@ namespace ManagerBoot.Admin
     {
         public Startup(IHostingEnvironment env)
         {
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables()
+            //    .AddJsonFile("diservice.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile("connectionStrings.json", optional:true, reloadOnChange:true);
+            //Configuration = builder.Build();
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables()
-                .AddJsonFile("diservice.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("connectionStrings.json", optional:true, reloadOnChange:true);
+                .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
 
@@ -33,43 +41,36 @@ namespace ManagerBoot.Admin
             // Add framework services.
             services.AddMvc();
 
-            //services.AddSession(option =>
+            ////配置服务依赖注入
+            //var requiredServices = new List<DIService>();
+            //Configuration.GetSection("DIServices")?.Bind(requiredServices);
+            //if (requiredServices == null || requiredServices.Count == 0)
             //{
-            //    option.IdleTimeout = TimeSpan.FromMinutes(30);
+            //    throw new Exception("注入服务配置有误，请检查服务配置！");
+            //}
+            ////加载程序集
+            //Assembly asmb_IService = Assembly.Load(new AssemblyName("NetCoreBoot.IService"));
+            //Assembly asmb_Service = Assembly.Load(new AssemblyName("NetCoreBoot.Service"));
+            //requiredServices.ForEach(rservice =>
+            //{
+            //    services.Add(new ServiceDescriptor(serviceType: asmb_IService.GetType(rservice.ServiceType),
+            //                                       implementationType: asmb_Service.GetType(rservice.ImplementationType),
+            //                                       lifetime: rservice.Lifetime));
             //});
 
-            //配置服务依赖注入
-            var requiredServices = new List<DIService>();
-            Configuration.GetSection("DIServices")?.Bind(requiredServices);
-            if (requiredServices == null || requiredServices.Count == 0)
-            {
-                throw new Exception("注入服务配置有误，请检查服务配置！");
-            }
-            //加载程序集
-            Assembly asmb_IService = Assembly.Load(new AssemblyName("NetCoreBoot.IService"));
-            Assembly asmb_Service = Assembly.Load(new AssemblyName("NetCoreBoot.Service"));
-            requiredServices.ForEach(rservice =>
-            {
-                services.Add(new ServiceDescriptor(serviceType: asmb_IService.GetType(rservice.ServiceType),
-                                                   implementationType: asmb_Service.GetType(rservice.ImplementationType),
-                                                   lifetime: rservice.Lifetime));
-            });
+            ////注入HttpContextAccessor
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            //注入HttpContextAccessor
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            //注入公共操作服务
-            services.AddTransient<WebHelper>();
-
-
+            ////注入公共操作服务
+            //services.AddTransient<WebHelper>();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider isp)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
@@ -82,29 +83,31 @@ namespace ManagerBoot.Admin
             }
 
             app.UseStaticFiles();
-            NetCoreBoot.Common.HttpContext.ServiceProvider = isp;
-            NetCoreBoot.Data.DbContextProvider.Configuration = Configuration;
+            //NetCoreBoot.Common.HttpContext.ServiceProvider = isp;
+            //NetCoreBoot.Data.DbContextProvider.Configuration = Configuration;
             app.UseStaticFiles();
             //app.UseSession();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action}.html"
-                    );
+            app.UseMvc(ConfigureRoutes);
+        }
 
-                routes.MapRoute(
-                    name: "api",
-                    template: "{controller}/{action}.do"
-                    );
+        private void ConfigureRoutes(IRouteBuilder builder)
+        {
+            builder.MapRoute(
+                name: "default",
+                template: "{controller}/{action}.html"
+            );
 
-                routes.MapRoute(
-                    name: "replenish",
-                    template: "{controller=Login}/{action=Index}/{id?}"
-                    );
+            builder.MapRoute(
+                name: "api",
+                template: "{controller}/{action}.do"
+            );
 
-            });
+            builder.MapRoute(
+                name: "replenish",
+                template: "{controller=Login}/{action=Index}/{id?}"
+            );
+
         }
     }
 }
